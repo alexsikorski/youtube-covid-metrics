af9bd157b7491548b4128ae5c16ca12d9f6a620b
alexsikorski <alexsikorski22@gmail.com>
2020-07-18 20:24:46 +0100

    Iteration through loaded pickle file, values for metrics can be obtained.

--- analyse_data.py
+++ analyse_data.py
@@ -8,14 +8,28 @@ def load_pkl(name):
 
 
 def main():
+    # Statistics
+    video_count = 0
+    total_views = 0
+    # Statistics
+
     pkl_count = 0
     videos = []
     for subdir, dirs, files in os.walk(os.getcwd()):
         for file in files:
             if file.endswith(".pkl"):
                 pkl_count += 1
-                videos.append(load_pkl(subdir + "/" + file))
+                videos = videos + load_pkl(subdir + "/" + file)
 
+    video_count = len(videos)
+
+    for video in videos:
+        for (k, v) in video.items():
+            if k == "views":
+                total_views += int(v)
+
+    print(total_views)
+    print("Average views:", total_views/video_count)
 
 if __name__ == "__main__":
     main()
