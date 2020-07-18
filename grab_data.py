83910c5c08c78c9967b179ed5ffb87a8e53a61c3
alexsikorski <alexsikorski22@gmail.com>
2020-07-18 12:21:50 +0100

    Added grab_data.py that grabs generic video data of top videos

---
+++ grab_data.py
@@ -0,0 +1,80 @@
+import json
+import os
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+
+def main():
+    create_folder("./data/")
+
+    page_count = 1
+    file_name = "data-"
+
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    first_request = youtube.videos().list(
+        part="snippet,contentDetails,statistics,topicDetails",
+        chart="mostPopular",
+        maxResults=50,
+        regionCode="US"
+    )
+    first_response = first_request.execute()
+
+    with open("data/" + file_name + str(page_count) + ".json", "w") as outfile:
+        json.dump(first_response, outfile)
+
+    while True:
+        next_page = obtain_page("data/" + file_name + str(page_count) + ".json")
+        if next_page is None:
+            break
+        request = youtube.videos().list(
+            part="snippet,contentDetails,statistics,topicDetails",
+            chart="mostPopular",
+            maxResults=50,
+            pageToken=str(next_page),
+            regionCode="US"
+        )
+        response = request.execute()
+        print("\rWriting file data-" + str(page_count) + ".json", end="")
+        page_count = page_count + 1
+
+        with open("data/" + file_name + str(page_count) + ".json", "w") as outfile:
+            json.dump(response, outfile)
+
+    print("\rWriting file data-" + str(page_count) + ".json... All done!", end="")
+
+
+def obtain_page(data_file):
+    f = open(data_file)
+    data = json.load(f)
+    for (k, v) in data.items():
+        if k == "nextPageToken":
+            return v
+
+
+def create_folder(path):
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+    except OSError:
+        print('Error when creating directory. ' + path)
+
+
+if __name__ == "__main__":
+    main()
