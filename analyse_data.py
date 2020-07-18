deea07e8b3c00e4cf84aaac4a2fa8d9b25e42ed7
alexsikorski <alexsikorski22@gmail.com>
2020-07-18 21:46:02 +0100

    Covid related videos are now isolated and statistics are obtained

--- analyse_data.py
+++ analyse_data.py
@@ -11,25 +11,69 @@ def main():
     # Statistics
     video_count = 0
     total_views = 0
+    total_comments = 0
+    total_likes = 0
+    total_dislikes = 0
+
+    c_video_count = 0
+    c_total_views = 0
+    c_total_comments = 0
+    c_total_likes = 0
+    c_total_dislikes = 0
     # Statistics
 
     pkl_count = 0
     videos = []
+    covid_videos = []
+
     for subdir, dirs, files in os.walk(os.getcwd()):
         for file in files:
             if file.endswith(".pkl"):
                 pkl_count += 1
-                videos = videos + load_pkl(subdir + "/" + file)
+                videos += load_pkl(subdir + "/" + file)
 
     video_count = len(videos)
 
+    # for averages
     for video in videos:
         for (k, v) in video.items():
-            if k == "views":
-                total_views += int(v)
+            if v is not None:
+                if k == "views":
+                    total_views += int(v)
+                if k == "comments":
+                    total_comments += int(v)
+                if k == "likes":
+                    total_likes += int(v)
+                if k == "dislikes":
+                    total_dislikes += int(v)
 
-    print(total_views)
+    # isolate COVID-19 related videos
+    for video in videos:
+        for(k, v) in video.items():
+            if k == "title":
+                temp_lower = v.lower()
+                if "covid-19"in temp_lower or "covid" in temp_lower or "coronavirus" in temp_lower or "quarantine" in temp_lower:
+                    covid_videos.append(video)
+
+    c_video_count = len(covid_videos)
+
+    # for COVID-19 averages
+    for video in covid_videos:
+        for (k, v) in video.items():
+            if v is not None:
+                if k == "views":
+                    c_total_views += int(v)
+                if k == "comments":
+                    c_total_comments += int(v)
+                if k == "likes":
+                    c_total_likes += int(v)
+                if k == "dislikes":
+                    c_total_dislikes += int(v)
+
     print("Average views:", total_views/video_count)
+    print("Average COVID-19 views:", c_total_views/c_video_count)
+    
+
 
 if __name__ == "__main__":
     main()
